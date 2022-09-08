0c1680c6d62a71054e6cdbeca58a55ee5e49a921
Lew Krehnbrink <lew.krehnbrink@gmail.com>
2022-09-08 15:17:21 -0500

    remove boilerplate

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,8 @@ import githubIcon from 'bootstrap-icons/icons/github.svg';
 import linkedInIcon from 'bootstrap-icons/icons/linkedin.svg';
 
 export default function Header() {
+    const iconSize = 24;
+
     const navStyle: any = {
         display: 'flex', 
         margin: '1rem auto', 
@@ -14,23 +16,23 @@ export default function Header() {
         gap: '1rem'
     };
 
-    const imageSize = 24;
-
     return (
-        <nav style={navStyle}>
-            <Image src='/lew@2x.jpeg' alt='Lew Avatar' width='60' height='60' style={{borderRadius: '50%', flexShrink: 0}}></Image>
-            <div style={{textAlign: 'center'}}>
-                <h1 style={{margin: '0 0 .5rem', fontSize: '3rem'}}>Lew Krehnbrink</h1>
-                <p><u>Front-end Web Dev</u><br/>with<br/><Counter startTime='2015-05-01T08:00:00-05:00'/><br/>of full stack web development experience.</p>
-                <div style={{display: 'flex', gap: '1rem', justifyContent: 'center'}}>
-                    <a href="https://www.linkedin.com/in/lew-krehnbrink-6184a239" target="_blank" title='Lew on linkedIn' rel="noreferrer">
-                        <Image alt='LinkedIn Logo' {...linkedInIcon} width={imageSize} height={imageSize}/>
-                    </a>
-                    <a href="https://github.com/lkrehnbrink" target="_blank" title='Lew on github' rel="noreferrer">
-                        <Image alt='Github Logo' {...githubIcon} width={imageSize} height={imageSize}/>
-                    </a>
+        <header>
+            <nav style={navStyle}>
+                <Image src='/lew@2x.jpeg' alt='Lew Avatar' width='60' height='60' style={{borderRadius: '50%', flexShrink: 0}}></Image>
+                <div style={{textAlign: 'center'}}>
+                    <h1 style={{margin: '0 0 .5rem', fontSize: '3rem'}}>Lew Krehnbrink</h1>
+                    <p><u>Front-end Web Dev</u><br/>with<br/><Counter startTime='2015-05-01T08:00:00-05:00'/><br/>of full stack web development experience.<br/>Located in Chicago, IL.</p>
+                    <div style={{display: 'flex', gap: '1rem', justifyContent: 'center'}}>
+                        <a href="https://www.linkedin.com/in/lew-krehnbrink-6184a239" target="_blank" title='Lew on linkedIn' rel="noreferrer">
+                            <Image alt='LinkedIn Logo' {...linkedInIcon} width={iconSize} height={iconSize}/>
+                        </a>
+                        <a href="https://github.com/lkrehnbrink" target="_blank" title='Lew on github' rel="noreferrer">
+                            <Image alt='Github Logo' {...githubIcon} width={iconSize} height={iconSize}/>
+                        </a>
+                    </div>
                 </div>
-            </div>
-      </nav>
+            </nav>
+      </header>
     );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>
+				{/* <h1 className={styles.title}>
 					Welcome to <a href='https://nextjs.org'>Next.js!</a>
 				</h1>
 
@@ -60,7 +60,7 @@ const Home: NextPage = () => {
 							with Vercel.
 						</p>
 					</a>
-				</div>
+				</div> */}
 			</main>
 
 			<footer className={styles.footer}>
