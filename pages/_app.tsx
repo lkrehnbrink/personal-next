81e4bdf728a4e36ec9ee58c78db5e28f2676ce09
Lew Krehnbrink <lew.krehnbrink@gmail.com>
2022-09-10 22:11:55 -0500

    include svg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import Header from '../components/header'
 import Head from 'next/head'
 
-import * as ga from '../lib/ga';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import * as ga from '../lib/ga'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -27,25 +27,27 @@ function MyApp({ Component, pageProps }: AppProps) {
   },[router.events]);
 
   return <>
-    {'production' === process.env.APP_ENV && <Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-    </Head>}
+    <Head>
+      {'production' === process.env.APP_ENV && <>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </>}
+    </Head>
     <Header />
     <Component {...pageProps} />
   </>
