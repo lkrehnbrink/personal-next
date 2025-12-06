c153cb7092a9d959a085c5c88b97b5f7e12a2b3c
Lew Krehnbrink <lew.krehnbrink@gmail.com>
2025-12-06 11:46:34 -0600

    add vercel analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
+import { Analytics } from '@vercel/analytics/next'
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 	return (
 		<div className={styles.container}>
+			<Analytics />
 			<Head>
 				<title>Lew Krehnbrink</title>
 				<meta name='description' content='Full-stack Web Developer' />
